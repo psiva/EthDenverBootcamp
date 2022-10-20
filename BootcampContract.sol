2db2807f597a1673069e3ae8208b8c29d1f653dc
Siva Puvvada <siva.puvvada@gmail.com>
2022-10-20 14:02:28 +0530

    Create BootcampContract.sol

---
+++ BootcampContract.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: None
+
+pragma solidity 0.8.17;
+
+
+contract BootcampContract {
+
+    uint256 number;
+    address owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+
+    function returnAddr() external view returns (address){
+        if(msg.sender == owner){
+            return 0x000000000000000000000000000000000000dEaD;
+        } 
+        return msg.sender;
+    }
+}
