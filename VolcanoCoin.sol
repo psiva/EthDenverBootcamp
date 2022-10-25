77d78c82e48caf29ba2b9dcab39de347f2ef6fed
Siva Puvvada <siva.puvvada@gmail.com>
2022-10-25 16:53:48 +0530

    updated import

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  @title Contract to implement functionality for management of VolcanoCoin
